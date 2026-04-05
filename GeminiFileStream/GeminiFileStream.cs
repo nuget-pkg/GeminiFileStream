0ee87f038fff7ce55362a2968112e77fcd6cd605
javacommons <javacommons@gmail.com>
2026-04-06 08:45:39 +0900

    .

--- GeminiFileStream/GeminiFileStream.cs
+++ GeminiFileStream/GeminiFileStream.cs
@@ -5,17 +5,12 @@ using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using Core;
 
 // ReSharper disable once CheckNamespace
-namespace Global;
+namespace Universal;
 // ReSharper disable once InconsistentNaming
 public class GeminiFileStream {
     public static int Add2(int a, int b) {
         return a + b;
-    }
-    public static string[] ShuffulStringArray(string[] arr) {
-        var cobj = CoreObject.FromObject(arr);
-        return cobj.Shuffle().AsStringArray!;
     }
 }
